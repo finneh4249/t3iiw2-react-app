00ba3b90f9c030f681984537d8dfb0610df93048
Ethan Cornwill <ethancornwill@outlook.com>
2025-01-25 11:03:40 +1100

    add testing setup with Vitest and Jest DOM support

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test:{
+    globals: true, // Allows the use of describe, expect, etc without importing them
+    environment: 'jsdom', // Tests will run in a JSDOM environment
+    setupFiles: './src/setupTest.js', // A file that will be executed before running tests
+  }
 })
